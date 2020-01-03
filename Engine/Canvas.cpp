f901b21d3f23743e3d7534be84a54fd14ea71fcf
Lew <lew.scott@outlook.com>
2020-01-03 21:19:37 +0000

    Raycasting fixed and working

--- Engine/Canvas.cpp
+++ Engine/Canvas.cpp
@@ -8,11 +8,13 @@ Canvas::Canvas()
 {
 	std::uniform_real_distribution<float> vDist(-2.0f, 2.0f);
 	//p.init(Vec2(xDist(rng), yDist(rng)), Vec2(vDist(rng), vDist(rng)));
-	p.init({ 400,300, }, { 0,0 });
+	p.init({ 600,300, }, { 0,0 });
 	p.initRays();
 
-	w[0].init({ 500,200 }, { 500,400 });
-	w[1].init({ 200,200 }, { 200,400 });
+	for (int i = 0; i < walls; ++i)
+	{
+		w[i].init({ xDist(rng),yDist(rng) }, { xDist(rng),yDist(rng) });
+	}
 
 
 }
@@ -31,12 +33,14 @@ void Canvas::calcIntersects()
 {
 	Vec2 P = { 0,0 };
 	Vec2 Old = { 0,0 };
+	Vec2 New = { 0,0 };
 	for (int i = 0; i < p.getNumOfRays(); i++)
 	{
 		p.updatePFound(false);
-		P = p.getRayDir(i) * 5000;
+		
 		for (int j = 0; j < walls; j++)
 		{
+			P = p.getRayDir(i) * 5000;
 			P = lineIntersection(p.getPos(), P, w[j].getP1(), w[j].getP2());
 			if (P.x != 0 && P.y != 0 && p.pointFound() == false)
 			{
@@ -45,8 +49,9 @@ void Canvas::calcIntersects()
 			}
 			else if (P.x != 0 && P.y != 0 && p.pointFound() == true)
 			{
-				Old = p.getRayPoint(i);
-				if (P.GetLengthSq() < Old.GetLengthSq())
+				Old = p.getRayPoint(i) - p.getPos();
+				New = P - p.getPos();
+				if (New.GetLengthSq() < Old.GetLengthSq())
 				{
 					p.updateRayPos(P, i);
 					p.updatePFound(true);
@@ -74,7 +79,6 @@ Vec2 Canvas::lineIntersection(Vec2 p1, Vec2 p2, Vec2 p3, Vec2 p4)
 	float uNum = -((p1.x - p2.x)*(p1.y - p3.y) - (p1.y - p2.y)*(p1.x - p3.x));
 	float uDen = ((p1.x - p2.x)*(p3.y - p4.y) - (p1.y - p2.y)*(p3.x - p4.x));
 	float u = uNum / uDen;
-
 	if (t > 0 && t < 1 && u > 0 && u < 1)
 	{
 		return { p1.x + t * (p2.x - p1.x), p1.y + t * (p2.y - p1.y) };
